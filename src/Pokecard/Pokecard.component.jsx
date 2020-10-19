ca1c856b85ddda4303038150e7de2310f5c244b8
just-sid <be10125.17@bitmesra.ac.in>
2020-10-20 03:54:09 +0530

    expressions inside functions

--- src/Pokecard/Pokecard.component.jsx
+++ src/Pokecard/Pokecard.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './Pokecard.styles.css'
 
 export const Pokecard = (props) => {
+    const Poke_API = 'https://raw.github.com/PokeAPI/sprites/master/sprites/pokemon/';
+    let imgSrc = `${Poke_API}${props.pokemonId}.png`;
     return (
         <div className = "Pokecard">   
             <p className="Pokecard-title">{props.pokemonName}</p>
-            <img alt="pokemons" src={`https://raw.github.com/PokeAPI/sprites/master/sprites/pokemon/${props.pokemonId}.png`} />
+            <img alt="pokemons" src={ imgSrc }/>
             <div className="Pokecard-data">{`Type: ${props.pokemonType}`}</div>
             <div className="Pokecard-data">{`EXP: ${props.pokemonExp}`}</div>
         </div>
